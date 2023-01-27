4d66b3cc1f6441dec952178443e77e6dcaf8d1ab
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-27 19:42:41 +0200

    A new commit

---
+++ Section1/CorePython/sort.py
@@ -0,0 +1,8 @@
+li = [9, 1, 8, 2, 7, 3, 6, 4, 5]
+
+# If you want to create a new list containing the sorted version of the original list, use the sorted() function.
+s_li = sorted(li)
+
+# But if you want to sort the original version of the  list, use the .sort() method on the list.
+
+print('Sorted Variable:\t', s_li)
