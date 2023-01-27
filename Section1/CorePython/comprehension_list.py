11d51c95317900b4ca7be38e63dcddedcad468e4
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-27 16:30:39 +0200

    A new commit

--- Section1/CorePython/comprehension_list.py
+++ Section1/CorePython/comprehension_list.py
@@ -33,6 +33,8 @@
 # my_list = [(letter, num) for letter in 'abcd' for num in range(4)]
 # print(my_list)
 
+#               Example 4
+
 #           The Zip Function
 
 # zip() - The zip function combines two different arrays in a single zip object, Then you have to typecast the result into a tuple.
@@ -44,3 +46,19 @@
 # result = zip(name, surname)
 
 # print(tuple(result))
+
+
+#   Making a dictinary from the zip function
+# names = ['Jude', 'jane', 'clark']
+# surnames = ['wayne', 'john', 'superman']
+
+# my_dict = {}
+# for name, surname in zip(names, surnames):
+#     my_dict[name] = surname
+# print(my_dict)
+
+
+# list comprehension method
+#          ||
+#         VVVV
+
