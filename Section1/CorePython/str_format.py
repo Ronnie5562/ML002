fd6dd9ef4975247d68e50a0abb3f1171550871cc
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-01-29 17:00:07 +0200

    Str_format

---
+++ Section1/CorePython/str_format.py
@@ -0,0 +1,17 @@
+
+# from math import pi
+
+# sentence = 'pi is equal to {:.2f}'.format(pi)
+# print(sentence)
+
+#  To add commas to big numbers
+#sentence = '1 MB is equal to {:,} bytes'.format(1000**2)
+
+#To format dates
+import datetime
+my_date = datetime.datetime(2016, 9, 24, 12, 30, 45)
+
+sentence_1 = '{:%B, %d, %Y}'.format(my_date)
+print(sentence_1) 
+
+sentence_2 = '{0:%B %d, %Y} fell on a {0:%A} and was the {0:%j} day of the year'.format(my_date)
--- Section1/CorePython/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-names = ['Jude', 'jane', 'clark', 'peter']
-# surnames = ['wayne', 'john', 'superman', 'Oyonda']
-
-# my_dict = {name: surname for name, surname in zip(names, surnames) if name != 'peter'}
