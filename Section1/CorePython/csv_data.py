e955c97fc50bf4c78041ae27cab41e960d6efde8
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-08 22:37:00 +0200

    new commit

--- Section1/CorePython/csv_data.py
+++ Section1/CorePython/csv_data.py
@@ -1,3 +1,24 @@
 import csv
 
-with open()
+# The normal way
+
+# with open('data.csv', 'r', encoding='UTF-8') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+
+#     with open('new_data.csv', 'w', encoding='UTF-8') as new_file:
+#         csv_writer = csv.writer(new_file, delimiter='-')
+
+#         for line in csv_reader:
+#             csv_writer.writerow(line)
+
+# Using DictReader and DictWriter
+
+with open('data.csv', 'r', encoding='UTF-8') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+
+    with open('new_data.csv', 'w', encoding='UTF-8') as new_file:
+        fieldnames = ['first_name', 'last_name', 'email']
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\n')
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
