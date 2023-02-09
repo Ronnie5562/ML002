8d8c6610a4706715af9f5d41bee3bbaa8a6644dd
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-09 11:55:06 +0200

    A new commit

--- Section1/CorePython/csv_data.py
+++ Section1/CorePython/csv_data.py
@@ -13,12 +13,28 @@ import csv
 
 # Using DictReader and DictWriter
 
-with open('data.csv', 'r', encoding='UTF-8') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+# with open('data.csv', 'r', encoding='UTF-8') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
 
-    with open('new_data.csv', 'w', encoding='UTF-8') as new_file:
-        fieldnames = ['first_name', 'last_name', 'email']
-        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\n')
+#     with open('new_data.csv', 'w', encoding='UTF-8') as new_file:
+#         fieldnames = ['first_name', 'last_name', 'email']
+#         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\n')
 
-        for line in csv_reader:
-            csv_writer.writerow(line)
+#         for line in csv_reader:
+#             csv_writer.writerow(line)
+
+
+# Code to prevent any entry with the firstname - {Tom} [Using  DictReader and DictWriter]
+
+# with open('data.csv') as csv_file:
+#     csv_reader = csv.DictReader(csv_file)
+
+#     with open('new_data.csv', 'w') as new_file:
+#         fieldnames = ['first_name', 'last_name', 'email']
+#         csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames,delimiter='\n')
+
+#         #next(csv_file)
+#         for line in csv_reader:
+#             if line['first_name'] == 'Tom':
+#                 break
+#             csv_writer.writerow(line)
