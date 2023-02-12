14ea0c22a9683af629014f0c1728d84bdd112497
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-12 18:20:01 +0200

    Exception.py

--- Section1/CorePython/Exception.py
+++ Section1/CorePython/Exception.py
@@ -8,6 +8,7 @@
 # 
 
 
+#_____{BUILTIN ERROR}_____
 
 # try:
 #     f = open('Calc.py')
@@ -16,3 +17,15 @@
 #     print(NOTFOUND)
 # except Exception as UNKNOWNERROR:
 #     print(UNKNOWNERROR)
+
+# With the "else-block" ==> [if the code in the try-block does not throw an error, the program runs the code in the else-block]
+# try:
+#     f = open('Calc.py')
+# except FileNotFoundError as NOTFOUND:
+#     print(NOTFOUND)
+# except Exception as UNKNOWNERROR:
+#     print(UNKNOWNERROR)
+# else:
+#     print(f.read())
+#     f.close()
+#     print(f.closed)
