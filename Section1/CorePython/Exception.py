eb2333130de02b51cc359bec6c4e225f15299a62
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-12 18:15:16 +0200

    Exception.py

---
+++ Section1/CorePython/Exception.py
@@ -0,0 +1,18 @@
+# try:
+#     f = open('Calc.py')
+#     var = bad_var
+# except FileNotFoundError:
+#     print('sorry. This file does not exist')
+# except Exception:
+#     print('sorry. something went wrong')
+# 
+
+
+
+# try:
+#     f = open('Calc.py')
+#     var = bad_var
+# except FileNotFoundError as NOTFOUND:
+#     print(NOTFOUND)
+# except Exception as UNKNOWNERROR:
+#     print(UNKNOWNERROR)
