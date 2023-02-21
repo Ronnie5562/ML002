11aa63331461eb722097c6cbaa5af5b344baea7a
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-21 17:08:01 +0200

    filter_map.py ✅

---
+++ Section1/CorePython/higher_order.py
@@ -0,0 +1,44 @@
+#filter() function:
+# We can use filter() function to filter values from the given sequence based on some condition.
+#
+# filter(function, sequence)
+#
+# where function argument is responsible to perform conditional check and the sequence can be list or tuple or string.
+
+
+# Q. Program to filter only even numbers from the list by using filter() function?
+
+# without lambda Function:
+def isEven(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+    
+l=[0,5,10,15,20,25,30] 
+l1=list(filter(isEven,l)) 
+print(l1) #[0,10,20,30]
+
+#with lambda Function:
+l=[0,5,10,15,20,25,30] 
+l1=list(filter(lambda x:x%2==0,l)) 
+print(l1) #[0,10,20,30] 
+l2=list(filter(lambda x:x%2!=0,l)) 
+print(l2) #[5,15,25]
+
+
+# map() function:
+# For every element present in the given sequence, apply some functionality and generate
+# new element with the required modification. For this requirement we should go for
+# map() function.
+# Eg: For every element present in the list perform double and generate new list of doubles.
+# Syntax:
+# map(function, sequence)
+# The function can be applied on each element of sequence and generates new sequence
+
+
+numbers = [1,2,3,4,5,6,7,8]
+
+new_numbers = list(map(lambda x : x * x, numbers))
+
+print(new_numbers)
