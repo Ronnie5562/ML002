cd116da70a1e9d88a1d109a0a00f469771acf656
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-02-27 01:34:52 +0200

    scope.py ✅

--- Section1/CorePython/scope.py
+++ Section1/CorePython/scope.py
@@ -4,6 +4,7 @@ B -> E -> G -> L ❌
 
 Local, Enclosing, Global, Built-in.
     Local - Variables defined inside a functon.
+    Enclosing - Variables in a function - which is inside another function
     Global - Variables defined at the top level of a module.
     Built-in - Variables that are pre-defined in python.
 '''
@@ -24,3 +25,7 @@ scope()
 print(x)
 # print(y) ==> it throws an error ❌❌❌
 print(z)
+
+# To check all the built-in funcs and variables in python
+import builtins
+print(dir(builtins)) # output - All built-ins in python.
