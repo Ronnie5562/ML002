e400008f997059ac87eecc67331047b06a284302
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-03 03:55:45 +0200

    Counter.py

--- Section1/Real-World/counter.py
+++ Section1/Real-World/counter.py
@@ -8,3 +8,26 @@ payment_details = Counter(groceries)
 print(payment_details.most_common()) # Prints the out in according to the number of times they appear descending order.add()
 print(payment_details.most_common(1)) # Prints out the element that appears the most.
 
+
+from collections import Counter
+def word_counter(data):
+    with open('dummy.txt') as file:
+        file_data = file.read()
+        file_data_array = file_data.split()
+        new_file_data_array = []
+
+        for word in file_data_array:
+            word = word.strip('.')
+            word = word.strip(',')
+            new_file_data_array.append(word)
+
+        words_counter = Counter(new_file_data_array)
+        dict_data = dict(words_counter)
+
+        if data in set(dict_data.keys()):
+            print(dict_data[data])
+        else:
+            print('Word Not Found!!')
+
+
+word_counter('vocabulary')
