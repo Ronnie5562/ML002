646b1d3da3e496e181e3968a77c25511790b2ea0
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-03-09 22:47:36 +0200

    Unittest.py

--- Section1/Real-World/test_for_test.py
+++ Section1/Real-World/test_for_test.py
@@ -8,9 +8,10 @@ def setUpModule():
 def tearDownModule():
     print('tearDownModule')
 
+
 if __name__ == '__main__':
     test_.setUpModule = setUpModule
     test_.tearDownModule = tearDownModule
-    suite1 = unittest.TestLoader().loadTestsFromTestCase(TestFib)
-    suite = unittest.TestSuite([suite1])
+    Loaded_Suite = unittest.TestLoader().loadTestsFromTestCase(TestFib)
+    suite = unittest.TestSuite([Loaded_Suite])
     unittest.TextTestRunner().run(suite)
