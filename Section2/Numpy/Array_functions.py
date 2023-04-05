4849d9b2f04ce8d887b8dab293216c324fbb7f76
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-05 18:26:12 +0200

    Started learning about Numpy array functions

---
+++ Section2/Numpy/Array_functions.py
@@ -0,0 +1,8 @@
+"""__Numpy Array Functions__
+1. Array() ==>  It creates an Array from lists or tuples.
+"""
+
+import numpy as np
+#  1. Array():
+# To get info about the numpy array() function, run ==> print(help(np.array))
+
