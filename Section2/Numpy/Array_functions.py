55f92518986825e838c3e4877a35b58fda4cce9b
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-05 18:44:02 +0200

    numpy array methods

--- Section2/Numpy/Array_functions.py
+++ Section2/Numpy/Array_functions.py
@@ -6,3 +6,13 @@ import numpy as np
 #  1. Array():
 # To get info about the numpy array() function, run ==> print(help(np.array))
 
+li1 = np.array([1,2,3,4,])
+li2 = np.array([1,2,3,4,2.5,3.7])
+li3 = np.array([1,2,3,4,2.5,3.7,'d'])
+print(li1)
+print(li2)
+print(li3)
+
+# Run the code above and carefully observe the data type of the elements in each array. What can you say about this?
+# Basically, the type is determined as the minimum type required to hold the objects in the sequence.
+
