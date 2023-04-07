ff47821408d15093c819476901ae438fedb1c560
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-07 20:22:30 +0200

    Numpy slicing

--- Section2/Numpy/indexing&slicing.py
+++ Section2/Numpy/indexing&slicing.py
@@ -26,10 +26,14 @@ Three_Dim_Matrix = np.array([
 
 #   INDEXING IN NUMPY
 
+print('_____________________{ INDEXING }________________________')
+print()
+
 print(Row_Matrix.ndim, 'Dimensional Matrix')
 X = Row_Matrix[4]
 Y = Row_Matrix[-1]
 print(X * Y)
+
 
 print(Two_Dim_Matrix.ndim, 'Dimensional Matrix')
 X = Two_Dim_Matrix[0][-1]
@@ -42,5 +46,41 @@ X = Three_Dim_Matrix[0][1][3]
 Y = Three_Dim_Matrix[1][-2][-4]
 print(X * Y)
 
+print('_________________________________________________________')
+
+
 
 #   SLICING IN NUMPY
+
+print('_____________________{ SLICING }________________________')
+print()
+
+print(Row_Matrix.ndim, 'Dimensional Matrix')
+#   SYNTAX ==> array_name[start:stop:step]
+X = Row_Matrix[2:6]
+Y = Row_Matrix[3::2]
+print(X)
+print('______________________')
+print(Y)
+print()
+
+
+print(Two_Dim_Matrix.ndim, 'Dimensional Matrix')
+#   SYNTAX ==> array_name[start:stop:step, start:stop:step]
+X = Two_Dim_Matrix[1:, 1:]
+Y = Two_Dim_Matrix[:, -3::-1]
+print(X)
+print('______________________')
+print(Y)
+print()
+
+
+print(Three_Dim_Matrix.ndim, 'Dimensional Matrix')
+#   SYNTAX ==> array_name[start:stop:step, start:stop:step, start:stop:step]
+X = Three_Dim_Matrix[:, :, -1:]
+Y = Three_Dim_Matrix[:, 1:, ::2]
+print(X)
+print('______________________')
+print(Y)
+
+print('_________________________________________________________')
