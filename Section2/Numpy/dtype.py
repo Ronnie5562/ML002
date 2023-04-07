cf132e55c6767dc3d72f325dc3efbd98a9ce1d2a
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-07 19:00:56 +0200

    Numpy data types

---
+++ Section2/Numpy/dtype.py
@@ -0,0 +1,15 @@
+"""__ DATA TYPES IN NUMPY __
+(1) '?' ==> Boolean
+(2) 'b' ==> (signed) byte
+(3) 'B' ==> unsigned byte
+(4) 'i' ==> (signed) int
+(5) 'u' ==> unsigned int
+(6) 'f' ==> floating point
+(7) 'c' ==> complex-floating point
+(8) 'm' ==> timedelta
+(9) 'M' ==> datetime
+(10) 'O' ==> (python) objects
+(11) 'U' ==> Unicode string
+(12) 'V' ==> raw data (void)
+(13) 'S', 'a' ==> Zero-terminated bytes (not recommended)
+"""
