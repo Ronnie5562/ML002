b0317a607a7eef2c1d29845b088a40d296cc634a
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-04-19 21:36:42 +0200

    Pandas is cool

--- Section1/Coding_challenge/password.py
+++ Section1/Coding_challenge/password.py
@@ -2,7 +2,11 @@
     This modules runs a code that prints out a password {hub_password} which I saved as an environment variable on my system.
     This is useful in cases where you want to hide some informations in your code such as database passwords e.t.c.
 """
-import os
+# import os
 
-hub_password = os.environ.get('HUB_PASSWORD')
-print(hub_password)
+# hub_password = os.environ.get('HUB_PASSWORD')
+# print(hub_password)
+
+from os import getenv
+x = getenv('HUB_PASSWORD')
+print(x)
