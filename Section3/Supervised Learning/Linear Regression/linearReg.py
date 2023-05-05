ceab5e57d4a04bd67fd67be32f1852cefaa78592
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-05-05 17:51:06 +0200

    R_squared

--- Section3/Supervised Learning/Linear Regression/linearReg.py
+++ Section3/Supervised Learning/Linear Regression/linearReg.py
@@ -1,8 +1,8 @@
 """_summary_
-In this module, I learnt 
-       _ _        __  
+In this module, I learnt how linear regression works under_the_hood!!!
+              _ _       __  
 m(slope) = __(X.Y)__-__(XY)__   <== The formular
-       (X)^2 - X^2
+                (X)^2 - X^2
                  _    _
 b(y-intercept) = y - mx
 """
@@ -13,7 +13,7 @@ import numpy as np
 style.use("fivethirtyeight")
 
 X_axis = np.array([1, 2, 3, 4, 5, 6, 7], dtype=np.float64)
-Y_axis = np.array([5, 4, 6, 5, 6, 7, 8], dtype=np.float64)
+Y_axis = np.array([4, 5, 6, 7, 8, 9, 10], dtype=np.float64)
 
 def best_fit_slope(X, Y):
     m = (
@@ -32,6 +32,21 @@ Y_intercept = y_intercept(X_axis, Y_axis, Slope)
 
 regression_line = [((Slope * X) + Y_intercept) for X in X_axis]
 print(regression_line)
+
+def squared_error(ys_orig, ys_line):
+    return sum((ys_orig - ys_line)**2)
+
+
+def coefficient_of_determination(ys_orig, ys_line):
+    y_mean_line = [mean(ys_orig) for x in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+
+r_squared = coefficient_of_determination(Y_axis, regression_line)
+print(r_squared)
+
 
 plt.scatter(X_axis, Y_axis, marker='X', color='#003F72', label='data')
 plt.plot(X_axis, regression_line, label='regression line')
