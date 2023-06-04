be6fe929e126d692a7eddbc8092c3729496d5f77
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-04 17:11:15 +0200

    Python �

--- Section1/Django/django_project/blog/views.py
+++ Section1/Django/django_project/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-from 
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Blog Home Page</h1>') 
