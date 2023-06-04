9346c3326e3ac1588d85de3eb6e14bbc09fe6047
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-04 22:22:18 +0200

    Django is cool

--- Section1/Django/django_project/blog/urls.py
+++ Section1/Django/django_project/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home')
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about')
 ]
--- Section1/Django/django_project/blog/views.py
+++ Section1/Django/django_project/blog/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
+
+posts = [
+    {
+        'title': 'Blog Post 1',
+        'author': 'Ronald Abimbola',
+        'date_posted': 'February 20, 2023',
+        'content': "This is blog post ONE"
+    },
+    {
+        'title': 'Blog Post 2',
+        'author': 'Benita Abimbola',
+        'date_posted': 'November 12, 2023',
+        'content': "This is blog post TWO"
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home Page</h1>') 
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
