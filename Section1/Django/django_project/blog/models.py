327968086a36d5bdebebf50287f8632d9bd499cf
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-11 20:14:02 +0200

    Learning in progress....

--- Section1/Django/django_project/blog/models.py
+++ Section1/Django/django_project/blog/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- Section1/Django/django_project/blog/views.py
+++ Section1/Django/django_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
@@ -21,7 +22,7 @@ posts = [
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
