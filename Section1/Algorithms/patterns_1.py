410a524039369803747fbdaf5790504794de3bae
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-13 21:13:13 +0200

    ML

--- Section1/Algorithms/patterns_1.py
+++ Section1/Algorithms/patterns_1.py
@@ -1,27 +1,27 @@
-# """Pattern 1"""
+"""Pattern 1"""
 
-# print("Pattern 1")
-# n = int(input("Enter the number of rows: "))
-# for i in range(1, n + 1):
-#     print("*" * n)
+print("Pattern 1")
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    print("*" * n)
 
-# """Pattern 2"""
+"""Pattern 2"""
 
-# print("Pattern 2")
-# n = int(input("Enter the number of rows: "))
-# for i in range(1, n + 1):
-#     for j in range(n):
-#         print(i, end="")
-#     print()
+print("Pattern 2")
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    for j in range(n):
+        print(i, end="")
+    print()
 
-# """Pattern 3"""
+"""Pattern 3"""
 
-# print("Pattern 3")
-# n = int(input("Enter the number of rows: "))
-# for i in range(n):
-#     for j in range(1, n + 1):
-#         print(j, end="")
-#     print()
+print("Pattern 3")
+n = int(input("Enter the number of rows: "))
+for i in range(n):
+    for j in range(1, n + 1):
+        print(j, end="")
+    print()
 
 """Pattern 4"""
 
@@ -51,3 +51,26 @@ for i in range(n):
     print()
 
 """Pattern 7"""
+
+print("Pattern 7")
+n = int(input("Enter the number of rows: "))
+for letter in range(1, n + 1):
+    for j in range(n):
+        print(chr(65 + n - letter), end="")
+    print()
+
+"""Pattern 8"""
+
+print("Pattern 8")
+n = int(input("Enter the number of rows: "))
+for i in range(n):
+    for letter in range(1, n + 1):
+        print(chr(65 + n - letter), end="")
+    print()
+
+"""Pattern 9"""
+print("Pattern 9")
+n = int(input("Enter the number of rows: "))
+for j in range(1, n + 1):
+    print("*" * j)
+print()
