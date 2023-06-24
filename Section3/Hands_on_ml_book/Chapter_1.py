2aa3b200832fd5e73c6eec4a5d9182be1f519905
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-06-24 16:32:57 +0200

    Ml

--- Section3/Hands_on_ml_book/Chapter_1.py
+++ Section3/Hands_on_ml_book/Chapter_1.py
@@ -1,2 +1,19 @@
 """Hands on ML"""
 """ End to end project """
+""" Code to download our dataset """
+import os
+import tarfile
+from six.moves import urllib
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+ if not os.path.isdir(housing_path):
+    os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+fetch_housing_data()
